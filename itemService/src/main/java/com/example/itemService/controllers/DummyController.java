f55a686f23fb3f56a9fc70f0a94871b25bbdc04a
RISHIKESH SHINDE <123rishinde321@gmail.com>
2025-06-05 19:27:54 +0530

    dummy controller added

---
+++ itemService/src/main/java/com/example/itemService/controllers/DummyController.java
@@ -0,0 +1,15 @@
+package com.example.itemService.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dummy")
+public class DummyController {
+
+    @GetMapping("/rushi")
+    public String getDummy(){
+        return "Hii";
+    }
+}
